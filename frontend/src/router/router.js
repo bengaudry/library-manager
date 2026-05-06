5dd9054ebc037242988a18eb39561d7f952ea430
bengaudry <bengaudry@outlook.fr>
2026-05-06 16:58:40 +0200

    inscription router

--- frontend/src/router/router.js
+++ frontend/src/router/router.js
@@ -1,13 +1,14 @@
-import {createRouter, createMemoryHistory} from "vue-router";
+import {createRouter, createWebHistory} from "vue-router";
 
 const routes = [
     { path: "/", component: () => import("../views/Accueil.vue") },
+    { path: "/lecteur/inscription", component: () => import("../views/lecteur/Inscription.vue") },
     { path: "/gestionnaire/reservations", component: () => import("../views/gestionnaire/Reservations.vue") },
     { path: "/ouvrages", component: () => import("../views/Ouvrages.vue") },
     { path: "/lecteur/reservations", component: () => import("../views/lecteur/Reservations.vue") },
 ]
 
 export const router = createRouter({
-    history: createMemoryHistory(),
+    history: createWebHistory(),
     routes
 })

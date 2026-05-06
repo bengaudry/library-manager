9347f5639f7f4d815205e9bf75fb33b723050a6a
GANA NAHEL p2306697 <p2306697@polytech12826.univ-lyon1.fr>
2026-05-06 16:13:41 +0200

    Reset backend to master version

---
+++ backend/app/models.py
@@ -0,0 +1,23 @@
+from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy import String
+from sqlalchemy.orm import Mapped, mapped_column
+
+db = SQLAlchemy()
+
+class Books(db.Model):
+    __tablename__ = "books"
+    id: Mapped[int] = mapped_column(primary_key=True)
+    title: Mapped[str] = mapped_column(String(80))
+    author: Mapped[str] = mapped_column(String(80))
+    exemplars: Mapped[int] = mapped_column(default=1)
+    position: Mapped[str] = mapped_column(String(80))
+
+    def to_dict(self):
+        return {
+            "id": self.id,
+            "title": self.title,
+            "author": self.author
+        }
+
+    def __repr__(self) -> str:
+        return f"User(id={self.id!r}, title={self.title!r}, author={self.author!r})"
---
+++ backend/app/routes.py
@@ -0,0 +1,17 @@
+from flask import Blueprint, jsonify, request
+from .models import db, Books
+
+api = Blueprint('api', __name__)
+
+@api.route('/books', methods=['GET'])
+def get_books():
+    books = db.session.query(Books).all()
+    return jsonify([b.to_dict() for b in books])
+
+@api.route('/books', methods=['POST'])
+def add_book():
+    data = request.get_json()
+    new_book = Books(title=data['title'], author=data['author'], position=data['position'], exemplars=data['exemplars'])
+    db.session.add(new_book)
+    db.session.commit()
+    return jsonify(new_book.to_dict()), 201

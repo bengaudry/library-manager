0e93c70ee1b3510f2f3012d96f09b1d287356a80
mael-grn <maelgarnier5@gmail.com>
2026-05-06 16:16:11 +0200

    Selection d'un livre spécifique

--- backend/app/routes.py
+++ backend/app/routes.py
@@ -8,6 +8,14 @@ def get_books():
     books = db.session.query(Books).all()
     return jsonify([b.to_dict() for b in books])
 
+
+@api.route('/books/<int:book_id>', methods=['GET'])
+def get_book(book_id):
+    book = db.session.query(Books).get(book_id)
+    if book is None:
+        return jsonify({"error": "Livre non trouvé"}), 404
+    return jsonify(book.to_dict())
+
 @api.route('/books', methods=['POST'])
 def add_book():
     data = request.get_json()
